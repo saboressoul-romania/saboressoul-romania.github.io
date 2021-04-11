244c85a142e654d4009928db011f3c4e4dde0824
Liviu Nedelcu <liviu.nedelcu@likezero.co.uk>
2021-04-11 19:37:38 +0100

    Create informations section

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-    * Start Bootstrap - Grayscale v6.0.3 (https://startbootstrap.com/theme/grayscale)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-    */
     (function ($) {
     "use strict"; // Start of use strict
 
@@ -53,4 +48,4 @@
     navbarCollapse();
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
-})(jQuery); // End of use strict
+})(jQuery);
